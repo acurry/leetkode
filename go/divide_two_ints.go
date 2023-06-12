e4078d68f16aa26b6457ad8c26b69a21fd52b265
Andrew Curry <acurry@users.noreply.github.com>
2023-06-11 22:12:26 -0400

    Squashed commit of the following:
    commit 49c361b613fcf622c017635535ac0894269c4223
    Author: Andrew Curry <acurry@users.noreply.github.com>
    Date:   Sun Jun 11 13:10:24 2023 -0400
    
        WIP on 29: 494 of 994 testcases passed
    
    commit 4f2c98333b189f4f1b883a349c6c1860dca680dc
    Author: Andrew Curry <acurry@users.noreply.github.com>
    Date:   Sun Jun 11 12:23:03 2023 -0400
    
        use atomic int to store concurrent result from conc_divide
    
    commit a15aa627ec3077d9a25ba38c405c9cbd2febd8b0
    Author: Andrew Curry <acurry@users.noreply.github.com>
    Date:   Sun Jun 11 12:22:33 2023 -0400
    
        WIP on 29

--- go/divide_two_ints.go
+++ go/divide_two_ints.go
@@ -1,41 +1,94 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+	"math"
+	"sync"
+	"sync/atomic"
 )
 
+const MAX_NEGATIVE int = -2_147_483_648
+const MAX_POSITIVE int = 2_147_483_647
+
 func divide(dividend int, divisor int) int {
-	val := float64(dividend / divisor)
-	valAsStr := fmt.Sprintf("%f", val)
-	whole := strings.Split(valAsStr, ".")[0]
+	a := int(math.Abs(float64(divisor)))
+	b := int(math.Abs(float64(dividend)))
 
-	ret, _ := strconv.ParseInt(whole, 10, 0)
-
-	return int(ret)
-}
-
-type args struct {
-	Dividend int
-	Divisor  int
-}
-
-func main() {
-	data := []args{
-		{
-			Dividend: 10,
-			Divisor:  3,
-		},
-		{
-			Dividend: 10,
-			Divisor:  -3,
-		},
+	/* edge cases */
+	/* MAX_POSITIVE / 1 */
+	if dividend >= MAX_POSITIVE && divisor == 1 {
+		return MAX_POSITIVE
+		/* MAX_NEGATIVE / 1 */
+	} else if dividend <= MAX_NEGATIVE && divisor == 1 {
+		return MAX_NEGATIVE
+	} else if dividend >= MAX_POSITIVE && divisor == -1 {
+		return MAX_NEGATIVE
+	} else if dividend <= MAX_NEGATIVE && divisor == -1 {
+		return MAX_POSITIVE
 	}
 
-	for _, a := range data {
-		v := divide(a.Dividend, a.Divisor)
-		fmt.Printf("divide(%d, %d): %d\n", a.Dividend, a.Divisor, v)
+	var quotient int32
+	if b >= MAX_POSITIVE {
+		quotient = conc_divide(b, a)
+	} else {
+		sum := a
+		quotient = 0
+		for sum <= b {
+			quotient += 1
+			sum += a
+		}
 	}
-	// fmt.Print("SOLVED!")
+
+	quotient_should_be_less_than_zero := (dividend < 0 && divisor > 0) ||
+		(dividend > 0 && divisor < 0)
+
+	if quotient_should_be_less_than_zero {
+		return int(0 - quotient)
+	} else {
+		return int(quotient)
+	}
 }
+
+func conc_divide(b, a int) int32 {
+	var wg sync.WaitGroup
+
+	var quotient int32
+
+	sub_dividend := b / a
+
+	for i := 0; i < a; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			sum := a
+			sub_quotient := 0
+			for sum <= sub_dividend {
+				sub_quotient += 1
+				sum += a
+			}
+
+			atomic.AddInt32(&quotient, int32(sub_quotient))
+		}()
+	}
+
+	wg.Wait()
+
+	return quotient
+}
+
+/* latest leetcode output */
+/*
+Wrong Answer
+
+494 / 994 testcases passed
+Input
+dividend =
+2147483647
+divisor =
+2
+Use Testcase
+Output
+1073741822
+Expected
+1073741823
+*/
---
+++ go/divide_two_ints_test.go
@@ -0,0 +1,87 @@
+package main
+
+import "testing"
+
+func Test_divide(t *testing.T) {
+	type args struct {
+		dividend int
+		divisor  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Two small positive ints",
+			args: args{
+				dividend: 10,
+				divisor:  3,
+			},
+			want: 3,
+		},
+		{
+			name: "Small positive dividend, small negative dividend",
+			args: args{
+				dividend: 10,
+				divisor:  -3,
+			},
+			want: -3,
+		},
+		{
+			name: "Small positive dividend, small negative dividend",
+			args: args{
+				dividend: 7,
+				divisor:  -3,
+			},
+			want: -2,
+		},
+		{
+			name: "Equal positive dividend and divisor",
+			args: args{
+				dividend: 1,
+				divisor:  1,
+			},
+			want: 1,
+		},
+		{
+			name: "Equal negative dividend and divisor",
+			args: args{
+				dividend: -1,
+				divisor:  -1,
+			},
+			want: 1,
+		},
+		{
+			name: "Max negative int32 dividend",
+			args: args{
+				dividend: -2147483648,
+				divisor:  -1,
+			},
+			want: 2147483647,
+		},
+		{
+			name: "Perf test 1",
+			args: args{
+				dividend: 2147483647,
+				divisor:  3,
+			},
+			want: 715827882,
+		},
+		{
+			name: "Perf test 2",
+			args: args{
+				dividend: 2147483647,
+				divisor:  2,
+			},
+			want: 1073741823,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := divide(tt.args.dividend, tt.args.divisor); got != tt.want {
+				t.Errorf("divide() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
